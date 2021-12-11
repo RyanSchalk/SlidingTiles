3108ac73a7f2d5adf489b9bfb7fbd806a9d67242
Ryan Schalk <ryandschalk@gmail.com>
2021-12-11 11:00:42 -0500

    When loading the puzzle, add all numbers and randomly shuffle them.

--- SlidingTiles/GameModel.swift
+++ SlidingTiles/GameModel.swift
@@ -13,23 +13,30 @@ let BOARD_DIMENSION = 3
 /**
  When instantiated, loads the initial game state. Models the state of the game as the player moves tiles.
  This includes the board, and will include the number of moves used and whether the player has won.
+ 
+ When initialized, the numbers on the board are randomly shuffled. Note that this may produce an unsolvable puzzle.
  */
 class GameModel: ObservableObject {
     /// A 2D array where indexing by column, then row gives the object at that space on the gameboard. Each space is either nil (empty) or contains a number.
     @Published var board: [[Int?]]
     
     init () {
-        self.board = {
-            var board: [[Int?]] = Array(
-                repeating: Array(repeating: nil, count: BOARD_DIMENSION),
-                count: BOARD_DIMENSION
-            )
-            
-            board[0][0] = 1
-            board[1][0] = 2
-            board[1][1] = 5
-            
-            return board
-        }()
+        var positions: [(Int, Int)] = []
+        for column in 0..<BOARD_DIMENSION {
+            for row in 0..<BOARD_DIMENSION {
+                positions.append((column, row))
+            }
+        }
+        
+        let shuffledTileNumbers = Array(1...BOARD_DIMENSION * BOARD_DIMENSION - 1).shuffled()
+        
+        self.board = Array(
+            repeating: Array(repeating: nil, count: BOARD_DIMENSION),
+            count: BOARD_DIMENSION
+        )
+        
+        zip(shuffledTileNumbers, positions).forEach { number, position in
+            self.board[position.0][position.1] = number
+        }
     }
 }
