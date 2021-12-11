2b4fcf027d3226aa69b4e4472548a358f6ef17d0
Ryan Schalk <ryandschalk@gmail.com>
2021-12-11 09:04:39 -0500

    Create project.

---
+++ SlidingTiles/SlidingTilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidingTilesApp.swift
+//  SlidingTiles
+//
+//  Created by Ryan Schalk on 12/13/21.
+//
+
+import SwiftUI
+
+@main
+struct SlidingTilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
